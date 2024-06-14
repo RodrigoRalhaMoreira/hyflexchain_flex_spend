8f2635d17104e90da8e84c5874a4d4283e30e1b8
RodrigoRalhaMoreira <rr.moreira@campus.fct.unl.pt>
2024-06-14 01:32:14 +0100

    test commit

---
+++ hyflexchain/src/main/java/pt/unl/fct/di/hyflexchain/planes/zero_knowledge_proofs/ZeroKnowledgeProofsInterface.java
@@ -0,0 +1,16 @@
+package pt.unl.fct.di.hyflexchain.planes.zero_knowledge_proofs;
+
+/**
+ * An interface to generate and validate zero-knowledge proofs.
+ */
+
+
+public class ZeroKnowledgeProofsInterface {
+    
+    public static ZeroKnowledgeProofsInterface getInstance()
+	{
+		return ZeroKnowledgeProofsInterface.getInstance();
+	}
+
+
+}
