cf11c09e58efa8c7b175991eb8c88d5c14d47cf0
RodrigoRalhaMoreira <rr.moreira@campus.fct.unl.pt>
2024-07-03 20:52:21 +0100

    incremental changes

---
+++ caliper/caliper-hyflexchain/lib/benchmarks/CreatePrivateTransactionWithoutSC.js
@@ -0,0 +1,92 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+const WorkloadModuleBase = require('@hyperledger/caliper-core').WorkloadModuleBase;
+
+const HyFlexChainPrivateTransaction = require("../connector/HyFlexChainPrivateTransaction");
+
+const Context = require("../connector/Context");
+
+const Util = require('../util/Util');
+
+/**
+ * Workload module for the benchmark round.
+ */
+class CreateTransactionWithoutScWorkload extends WorkloadModuleBase {
+    /**
+     * Initializes the workload module instance.
+     */
+    constructor() {
+        super();
+        this.txIndex = 0;
+    }
+
+    /**
+     * Assemble TXs for the round.
+     * @return {Promise<TxStatus[]>}
+     */
+    async submitTransaction() {
+        const originPubKey = "0x01" + this.sutContext.encodedPublicKey;
+        const destAddress = this.getRandDestAddress();
+        const val = Util.getRandomInt32();
+
+        const inputTxs = [HyFlexChainPrivateTransaction.createInputTx(this.getRandDestAddress(), "some hash", 0)];
+        const outputTxs = [HyFlexChainPrivateTransaction.createOutputTx(destAddress, val)];
+        const zkpProofData = [Buffer.from([1, 2, 3])];
+        const tx = new HyFlexChainPrivateTransaction(HyFlexChainPrivateTransaction.TRANSFER, originPubKey, inputTxs, outputTxs, zkpProofData);
+        tx.nonce = this.txIndex;
+
+        this.txIndex++;
+
+        return this.sutAdapter.sendRequests(tx);
+    }
+
+    /**
+     * Get a rand replica address from the array of destination addresses
+     * @return {string} random replica address
+     */
+    getRandDestAddress()
+    {
+        const destAddresses = this.sutContext.destAddresses;
+        const i = Util.getRandomInt(0, destAddresses.length);
+        return destAddresses[i];
+    }
+
+    /**
+     * Private function to get ZKP proof data.
+     * @return {Promise<Buffer[]>}
+     */
+    async getZkpProofData() {
+        const proofJsonData = await fs.readFile('proof.json', 'utf8');
+        const proof = JSON.parse(proofJsonData);
+        const provingKey = await fs.readFile('proving.key');
+        const publicInputs = ['input1', 'input2']; // Example, replace with actual inputs
+
+        // Construct zkpProofData based on the read and processed data
+        // Adjust according to your actual data structure and requirements
+        return [Buffer.from(provingKey), ...publicInputs.map(input => Buffer.from(input)), Buffer.from(JSON.stringify(proof))];
+    }
+}
+
+/**
+ * Create a new instance of the workload module.
+ * @return {WorkloadModuleInterface}
+ */
+function createWorkloadModule() {
+    return new CreateTransactionWithoutScWorkload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule;
---
+++ caliper/caliper-hyflexchain/lib/connector/HyFlexChainPrivateTransaction.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const Util = require('../util/Util');
+
+const Buffer = require('buffer').Buffer;
+
+/**
+ * Represents a HyFlexChain Node Transaction
+ */
+class HyFlexChainPrivateTransaction
+{
+	static TRANSFER = ["TRANSFER", Buffer.from([1])];
+
+	/**
+	 * Contract deployment transactions: a transaction that creates
+	 * and installs a smart contract on the chain. After being installed
+	 * it can be referenced in future transactions for execution.
+	 */
+	static CONTRACT_CREATE = ["CONTRACT_CREATE", Buffer.from([2])];
+
+	/**
+	 * Revoke a contract previously installed on the chain.
+	 * After this transaction is approved/executed it is no longer
+	 * possible to reference and execute the revoked smart contract.
+	 */
+	static CONTRACT_REVOKE = ["CONTRACT_REVOKE", Buffer.from([3])];
+
+	/**
+	 * An internal type used by HyFlexChain nodes to propose
+	 * committees for the future.
+	 */
+	static COMMITTEE_ELECTION = ["COMMITTEE_ELECTION", Buffer.from([4])];
+
+	/**
+	 * An internal type used by HyFlexChain nodes to rotate
+	 * the currently executing committee.
+	 */
+	static COMMITTEE_ROTATION = ["COMMITTEE_ROTATION", Buffer.from([5])];
+
+	
+	// ZKP transaction type
+
+	static ZKP_TRANSFER = ["ZKP_TRANSFER", Buffer.from([6])];
+	
+
+
+	
+
+	/**
+	 * Create a new transaction
+	 * @param {(string | Buffer)[]} txType
+	 * @param {Buffer} origin 
+	 * @param {object[]} inputTxs 
+	 * @param {object[]} outputTxs 
+	 * @param {Buffer []} zkpProofData
+	 */
+	constructor(txType, origin, inputTxs, outputTxs, zkpProofData)
+	{
+		this.version = "V1_0";
+		this.sender = {address : origin};
+		this.signatureType = undefined;
+		this.signature = undefined;
+		this.nonce = Util.getRandomInt(0, Number.MAX_SAFE_INTEGER);
+		this.transactionType = txType;
+		this.smartContract = undefined;
+		this.inputTxs = inputTxs;
+		this.outputTxs = outputTxs;
+		this.data = Buffer.alloc(1);
+		this.zkpType = HyFlexChainPrivateTransaction.ZKP_TRANSFER
+		this.zkpProofData = zkpProofData;
+	}
+
+
+	toJson()
+	{
+		return {
+            version : this.version,
+            sender : {address : this.sender.address.toString("base64")},
+            signatureType : this.signatureType,
+            signature : null,
+            nonce : this.nonce,
+            transactionType : this.transactionType[0],
+            smartContract : HyFlexChainPrivateTransaction.toJsonSmartContract(this.smartContract),
+            inputTxs : this.inputTxs.map(v => HyFlexChainPrivateTransaction.toJsonInputTx(v)),
+            outputTxs : this.outputTxs.map(v => HyFlexChainPrivateTransaction.toJsonOutputTx(v)),
+            data : this.data.toString("base64"),
+			zkpType : this.zkpType[0],
+			zkpProofData : this.zkpProofData.toString("base64")
+        }
+	}
+
+	static smartContract(ref, code)
+	{
+		return {id : {address : ref}, code : code};
+	}
+
+	static smartContractRef(ref)
+	{
+		return HyFlexChainPrivateTransaction.smartContract(ref, Buffer.alloc(0));
+	}
+
+	static smartContractCode(code)
+	{
+		return HyFlexChainPrivateTransaction.smartContract(Buffer.alloc(0), code);
+	}
+
+	static toJsonSmartContract(contract)
+	{
+		return {id : {address : contract.id.address.toString("base64")}, code : contract.code.toString("base64")};
+	}
+
+	static toJsonInputTx(inputTx)
+	{
+		return {
+			txId : inputTx.txId.toString("base64"),
+			outputIndex : inputTx.outputIndex
+		};
+	}
+
+	static toJsonOutputTx(outputTx)
+	{
+		return {recipient : {address : outputTx.recipient.address.toString("base64")}, value : outputTx.value};
+	}
+
+	static createInputTx(txHash, outputIndex)
+	{
+		return {
+			txId : txHash,
+			outputIndex : outputIndex
+		};
+	}
+
+	static createOutputTx(address, value)
+	{
+		return {recipient : {address : address}, value : value};
+	}
+	
+}
+
+module.exports = HyFlexChainPrivateTransaction;
